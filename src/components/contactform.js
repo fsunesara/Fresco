1cac0f8a851f856d9fcdb821a97012cad593e1c6
faisal <fss170000@utdallas.edu>
2022-10-15 10:21:49 -0500

    Attempt to fix forms

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -4,19 +4,19 @@ import { Link } from 'gatsby'
 export default class ContactForm extends Component {
     render() {
         return (
-            <form name="contact" netlify>
+            <form name="contact" method="POST" netlify>
                 <input type="hidden" name="form-name" value="contact" />
                 <div>
                     <label for="nameInput" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="nameInput"/>
+                    <input type="text" class="form-control" id="nameInput" name="name"/>
                 </div>
                 <div>
                     <label for="emailInput" class="form-label">Email Address</label>
-                    <input type="email" class="form-control" id="emailInput"/>
+                    <input type="email" class="form-control" id="emailInput" name="email"/>
                 </div>
                 <div>
                     <label for="messageInput" class="form-label">Message</label>
-                    <textarea class="form-control" id="messageInput"/>
+                    <textarea class="form-control" id="messageInput" name="message"/>
                 </div>
                 <button type="submit" class="btn btn-primary">Send</button>
             </form>
