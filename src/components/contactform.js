38581a2151f6dc469fa010066480974a6b91a63d
faisal <fss170000@utdallas.edu>
2022-10-19 10:02:14 -0500

    Make contact form fields required

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -7,15 +7,15 @@ export default class ContactForm extends Component {
                 <input type="hidden" name="form-name" value="contact" />
                 <div>
                     <label for="nameInput" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="nameInput" name="name"/>
+                    <input type="text" class="form-control" id="nameInput" name="name" required/>
                 </div>
                 <div>
                     <label for="emailInput" class="form-label">Email Address</label>
-                    <input type="email" class="form-control" id="emailInput" name="email"/>
+                    <input type="email" class="form-control" id="emailInput" name="email" required/>
                 </div>
                 <div>
                     <label for="messageInput" class="form-label">Message</label>
-                    <textarea class="form-control" id="messageInput" name="message"/>
+                    <textarea class="form-control" id="messageInput" name="message" required/>
                 </div>
                 <button type="submit" class="btn btn-primary">Send</button>
             </form>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,27 +27,29 @@ export default class NavBar extends Component {
     navBarAction = () => {
         this.state.open ? this.setState({open:false,classnames:"collapse navbar-collapse"}) : this.setState({open:true,classnames:"collapse navbar-collapse show"});
     }
+    
     render() {
-        return <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <div className="navbar-brand"/>
-        <button className="navbar-toggler" type="button" onClick={this.navBarAction}>
-            <span className="navbar-toggler-icon" />
-        </button>
-        <div className={this.state.classnames}>
-            <ul className="navbar-nav mx-auto">
-                {
-                    this.state.links.map(link => {
-                        return(
-                            <li key={link.id} className="nav-item">
-                                <Link to={link.path} className="nav-link text-capitalize">
-                                    {link.text}
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-        </nav>
+        return (
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="navbar-brand"/>
+                    <button className="navbar-toggler" type="button" onClick={this.navBarAction}>
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                <div className={this.state.classnames}>
+                    <ul className="navbar-nav mx-auto"> {
+                        this.state.links.map(link => {
+                            return(
+                                <li key={link.id} className="nav-item">
+                                    <Link to={link.path} className="nav-link text-capitalize">
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                    </ul>
+                </div>
+            </nav>
+        )
     }
 }
